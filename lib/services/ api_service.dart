a81ae4582549365a109e0beb40e3a975d00c7849
vasanth <vasanthkirshvasanthkirsh@gmail.com>
2024-08-26 17:33:12 +0530

    Refactor API handling to improve error management and modularity
    - Enhanced error handling in `ApiService` with specific handling for `ClientException`, `FormatException`, `Exception`, `Error`, and unknown errors.
    - Separated `Resource` classes into a dedicated file for better modularity.
    - Updated `LyricsService` and `SunriseSunsetService` to use the new `Resource` class for clean and consistent API responses.

--- lib/services/ api_service.dart
+++ lib/services/ api_service.dart
@@ -4,17 +4,15 @@ import '../models/ sunrise_sunset_model.dart';
 import '../models/lyrics_model.dart';
 
 
-abstract class Resource<Item> {}
+abstract class Resource<T> {}
 
-
-class Success<Item> extends Resource<Item> {
-  final Item data;
+class Success<T> extends Resource<T> {
+  final T data;
 
   Success(this.data);
 }
 
-
-class Failure<Item> extends Resource<Item> {
+class Failure<T> extends Resource<T> {
   final String? errorMessage;
   final Exception? exception;
   final Error? error;
@@ -22,25 +20,28 @@ class Failure<Item> extends Resource<Item> {
   Failure({this.errorMessage, this.exception, this.error});
 }
 
+
 class ApiService {
-  Future<Resource<Item>> fetchData<Item>(String url, Item Function(String) fromJson) async {
+  Future<Resource<T>> fetchData<T>(String url, T Function(Map<String, dynamic>) fromJson) async {
     try {
       final response = await http.get(Uri.parse(url));
 
       if (response.statusCode < 300) {
-        print("Response: ${response.body}");
-        return Success<Item>(fromJson(json.decode(response.body)));
+        final data = fromJson(json.decode(response.body) as Map<String, dynamic>);
+        return Success<T>(data);
       } else if (response.statusCode >= 500) {
         return Failure(errorMessage: "500: Server Error");
       } else {
         return Failure(errorMessage: "Error: ${response.statusCode} ${response.reasonPhrase}");
       }
     } on http.ClientException catch (e) {
-      return Failure(errorMessage: 'ClientException: ${e.message}');
+      return Failure(errorMessage: 'ClientException: ${e.message}', exception: e);
     } on FormatException catch (e) {
-      return Failure(errorMessage: 'FormatException: ${e.message}');
+      return Failure(errorMessage: 'FormatException: ${e.message}', exception: e);
     } on Exception catch (e) {
-      return Failure(errorMessage: 'Exception: ${e.toString()}');
+      return Failure(errorMessage: 'Exception: ${e.toString()}', exception: e);
+    } on Error catch (e) {
+      return Failure(errorMessage: 'Error: ${e.toString()}', error: e);
     } catch (e) {
       return Failure(errorMessage: 'Unknown error: ${e.toString()}');
     }
@@ -52,7 +53,7 @@ class LyricsService {
 
   Future<Resource<Lyrics>> fetchLyrics(String artist, String title) async {
     final url = 'https://api.lyrics.ovh/v1/$artist/$title';
-    return await _apiService.fetchData(url, (body) => Lyrics.fromJson(jsonDecode(body)));
+    return await _apiService.fetchData(url, (json) => Lyrics.fromJson(json));
   }
 }
 
@@ -61,6 +62,6 @@ class SunriseSunsetService {
 
   Future<Resource<SunriseSunset>> fetchSunriseSunset(double lat, double lng) async {
     final url = 'https://api.sunrise-sunset.org/json?lat=$lat&lng=$lng';
-    return await _apiService.fetchData(url, (body) => SunriseSunset.fromJson(jsonDecode(body)));
+    return await _apiService.fetchData(url, (json) => SunriseSunset.fromJson(json));
   }
 }
